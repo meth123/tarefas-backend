9fe04255aa9013b2b4a9a4a029b2372aa29c4a77
meth123 <mateusteixeira372@gmail.com>
2021-10-25 17:50:00 -0300

    Deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
 import routes from "./routes";
-
+ 
 const app = express()
 createConnection()
-
+ 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
-
-app.listen(3333)
+ 
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Servidor em Execução');
+});
